248963d8f99c9499920e6752dde1339982b02df4
I-Ting <it.tsai.1113@gmail.com>
2025-04-13 15:46:59 +0800

    add more utility methods

--- src/main/java/com/practice/blockchain/util/StringUtil.java
+++ src/main/java/com/practice/blockchain/util/StringUtil.java
@@ -1,7 +1,14 @@
 package com.practice.blockchain.util;
 
+import com.practice.blockchain.Transaction;
+
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.Signature;
+import java.util.ArrayList;
+import java.util.Base64;
 
 public class StringUtil {
 
@@ -21,5 +28,64 @@ public class StringUtil {
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public static byte[] applyECDSASignature (PrivateKey privateKey, String input) {
+        Signature dsa;
+        byte[] output = new byte[0];
+        try {
+            dsa = Signature.getInstance("ECDSA", "BC");
+            dsa.initSign(privateKey);
+            byte[] strByte = input.getBytes();
+            dsa.update(strByte);
+            byte[] realSignature = dsa.sign();
+            output = realSignature;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        return output;
+    }
+
+    public static boolean verifyECDSASignature(PublicKey publicKey, String data, byte[] signature) {
+        try {
+            Signature ecdsaVerify = Signature.getInstance("ECDSA", "BC");
+            ecdsaVerify.initVerify(publicKey);
+            ecdsaVerify.update(data.getBytes());
+
+            return ecdsaVerify.verify(signature);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    //Tacks in array of transactions and returns a merkle root
+    public static String getMerkleRoot(ArrayList<Transaction> transactions) {
+        int count = transactions.size();
+        ArrayList<String> previousTreeLayer = new ArrayList<>();
+
+        for (Transaction transaction : transactions) {
+            previousTreeLayer.add(transaction.transactionID);
+        }
+
+        ArrayList<String> treeLayer = previousTreeLayer;
+
+        while (count > 1) {
+            treeLayer = new ArrayList<>();
+
+            for (int i = 1; i < previousTreeLayer.size(); i++) {
+                treeLayer.add(applySha256(previousTreeLayer.get(i-1) + previousTreeLayer.get(i)));
+            }
+
+            count = treeLayer.size();
+            previousTreeLayer = treeLayer;
+        }
+        String merkleRoot = treeLayer.size() == 1 ? treeLayer.get(0) : "";
+
+        return merkleRoot;
+    }
+
+    public static String getStringFromKey(PublicKey key) {
+        return Base64.getEncoder().encodeToString(key.getEncoded());
     }
 }
