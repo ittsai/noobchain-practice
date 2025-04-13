dd252c0131f5f25d9b63d53f07ff8a78770eb942
I-Ting <it.tsai.1113@gmail.com>
2025-04-13 15:57:01 +0800

    update and add method

--- src/main/java/com/practice/blockchain/Block.java
+++ src/main/java/com/practice/blockchain/Block.java
@@ -2,18 +2,19 @@ package com.practice.blockchain;
 
 import com.practice.blockchain.util.StringUtil;
 
+import java.util.ArrayList;
 import java.util.Date;
 
 public class Block {
 
     public String hash;
     public String previousHash;
-    private String data;
+    public String merkleRoot;
+    private ArrayList<Transaction> transactions = new ArrayList<>();
     private long timeStamp;
     private int nonce;
 
-    public Block(String data, String previousHash) {
-        this.data = data;
+    public Block(String previousHash) {
         this.previousHash = previousHash;
         this.timeStamp = new Date().getTime();
 
@@ -21,14 +22,29 @@ public class Block {
     }
 
     public String calculateHash() {
-        return StringUtil.applySha256(previousHash + timeStamp + nonce + data);
+        return StringUtil.applySha256(previousHash + timeStamp + nonce + merkleRoot);
     }
 
+    // increase nonce value until hash target is reached
     public void mineBlock(int difficulty) {
-        String target = new String(new char[difficulty]).replace('\0', '0');
-        while(!hash.substring(0, difficulty).equals(target)) {
+        merkleRoot = StringUtil.getMerkleRoot(transactions);
+        String target = StringUtil.getDifficultyString(difficulty);
+        while (!hash.substring(0, difficulty).equals(target)) {
             nonce++;
             hash = calculateHash();
         }
+    }
+
+    public boolean addTransaction(Transaction transaction) {
+        if (transaction == null) return false;
+
+        if (!previousHash.equals("0")) {
+            if (!transaction.processTransaction()) {
+                return false;
+            }
+        }
+
+        transactions.add(transaction);
+        return true;
     }
 }
