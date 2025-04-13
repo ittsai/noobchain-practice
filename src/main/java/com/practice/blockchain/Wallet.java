6f0554a6a091255175ee9f16436722762261c4a2
I-Ting <it.tsai.1113@gmail.com>
2025-04-13 14:22:58 +0800

    add wallet

---
+++ src/main/java/com/practice/blockchain/Wallet.java
@@ -0,0 +1,28 @@
+package com.practice.blockchain;
+
+import java.security.*;
+import java.security.spec.ECGenParameterSpec;
+
+public class Wallet {
+    public PrivateKey privateKey; // sign
+    public PublicKey publicKey; // address
+
+    public Wallet() {
+
+    }
+
+    public void generateKeyPair() {
+        try {
+            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("ECDSA", "BC");
+            SecureRandom random = SecureRandom.getInstance("SHA1PRNG");
+            ECGenParameterSpec ecGenParameterSpec = new ECGenParameterSpec("prime192v1");
+
+            keyPairGenerator.initialize(ecGenParameterSpec, random);
+            KeyPair keyPair = keyPairGenerator.generateKeyPair();
+            privateKey = keyPair.getPrivate();
+            publicKey = keyPair.getPublic();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
