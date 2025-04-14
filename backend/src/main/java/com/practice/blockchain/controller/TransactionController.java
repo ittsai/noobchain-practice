9ae00a00ae950bab755cd78fe1c9c31988fbe3a6
I-Ting <it.tsai.1113@gmail.com>
2025-04-14 17:38:41 +0800

    add controller

---
+++ backend/src/main/java/com/practice/blockchain/controller/TransactionController.java
@@ -0,0 +1,14 @@
+package com.practice.blockchain.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "api/transaction", produces = MediaType.APPLICATION_JSON_VALUE)
+public class TransactionController {
+
+    @PostMapping("create")
+    public void createBlock() {}
+}
